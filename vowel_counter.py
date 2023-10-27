f76ad9769007043784e3bd663013a97a1b832f6c
kevingonzalez7997 <59447523+kevingonzalez7997@users.noreply.github.com>
2023-10-27 01:48:41 -0400

    Create vowel_counter.py

---
+++ vowel_counter.py
@@ -0,0 +1,12 @@
+def vowel_counter(word):
+    vowels = ['a','e','i','o','u']
+    vowel_count = 0
+    consonant_count = 0 
+    for letter in word:
+        if letter in vowels:
+            vowel_count += 1
+        elif letter.isalpha():
+            consonant_count += 1
+    print(f"The number of v is {vowel_count}")
+    print(f"The number of constonat is {consonant_count}")
+vowel_counter("Hello World")
